4a4c58b6a5fb4612832aa7aa83a9f46120e5405b
icamelxiehui <372623335@qq.com>
2017-07-27 09:52:31 +0800

    Revert "dele"
    This reverts commit fcd409d016439fb5c9ca6a641b8d3439703fa9fa.

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+File Name: test.py
+Author: xiehui
+mail: 372623335@qq.com
+Created Time: 四  7/27 09:38:11 2017
+"""
+
+
+if __name__ == "__main__":
+    print "OK"
