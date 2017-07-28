1f6b567ebcfa3a316ca792880d4a3e65714fc02f
icamelxiehui <372623335@qq.com>
2017-07-28 15:03:53 +0800

    modify models

--- engine/models.py
+++ engine/models.py
@@ -4,9 +4,14 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
-    title = models.CharField(max_length=200)
+    url = models.TextField()
+    image = models.TextField()
+    title = models.TextField()
     content = models.TextField()
-  #  pubtime = models.DateTimeField()
+    author = models.TextField()
+    website = models.TextField()
+    description = models.TextField()
+    pubtime = models.DateTimeField()
 
     def __str__(self):
         return self.title
